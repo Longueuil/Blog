d3bd6c33b05aaf467231d4e5346a645ee54ae6d6
Longueuil <vladimirtocarica@gmail.com>
2021-06-23 21:55:11 -0400

    -

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,12 +37,15 @@ public class BlogController{
         postRepository.save(post);
         return "redirect:/blog";
     }
-    // will receive dynamic parameter
+
     @GetMapping("/blog/{id}")
-    public String blogDetails(@RequestParam(value = "id") long id, Model model){
-       Optional<Post> post =  postRepository.findById(id);
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+//        if (postRepository.existsById(id)){
+//            return "redirect:/blog";
+//        }
+        Optional<Post> post =  postRepository.findById(id);
         ArrayList<Post> res = new ArrayList<>();
-        post.ifPresent(res :: add);
+        post.ifPresent(res::add);
         model.addAttribute("post", res);
         return "blog-details";
     }
