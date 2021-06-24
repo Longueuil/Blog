d12e3e9d7729380c50e8455c9f27ad70a656178a
Longueuil <vladimirtocarica@gmail.com>
2021-06-24 14:56:18 -0400

    - created mapping for edit blog in BlogController.java  - created blog-edit.html

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -40,14 +40,26 @@ public class BlogController{
 
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable(value = "id") long id, Model model){
-//        if (postRepository.existsById(id)){
-//            return "redirect:/blog";
-//        }
+        if (postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
         Optional<Post> post =  postRepository.findById(id);
         ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res::add);
         model.addAttribute("post", res);
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if (postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post =  postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
     }
 
 }
